8a557bb9b0248d29f518a71943642eae797fff52
Soham Kyada <skyada@team4099.com>
2022-12-26 20:23:02 -0500

    Created win conditions

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 let boardstate = 
 {
-    board_normal : [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    big_board : [0, 0, 0, 0, 0, 0, 0, 0, 0],
     full_board :
     {
         "board_a" : [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,16 +13,16 @@ let boardstate =
         "board_f" : [0, 0, 0, 0, 0, 0, 0, 0, 0],
         "board_g" : [0, 0, 0, 0, 0, 0, 0, 0, 0],
         "board_h" : [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        "board_i" : [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        "dud" : [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        "board_i" : [0, 0, 0, 0, 0, 0, 0, 0, 0]
         
     },
-    big_board : ["dud",[0, 0, 0, 0, 0, 0, 0, 0, 0]],
+    current_board : "",
+
 
     reset : function()
     {
         for (board in this.full_board) {
-            this.full_board[board] = this.board_normal
+            this.full_board[board] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         }
     }
 }
@@ -46,7 +46,7 @@ function openGame()
 //
 
 //important overall game functions
-let players_map =
+const players_map =
 {
     0 : "",
     "" : 0,
@@ -56,47 +56,71 @@ let players_map =
     "O" : 2
 }
 
-let player_colors = ["black", "red", "blue"];
+const board_map =
+{
+    "board_a" : 0,
+    "board_b" : 1,
+    "board_c" : 2,
+    "board_d" : 3,
+    "board_e" : 4,
+    "board_f" : 5,
+    "board_g" : 6,
+    "board_h" : 7,
+    "board_i" : 8,
+    0 : "board_a",
+    1 : "board_b",
+    2 : "board_c",
+    3 : "board_d",
+    4 : "board_e",
+    5 : "board_f",
+    6 : "board_g",
+    7 : "board_h",
+    8 : "board_i"
+}
 
+const player_colors = ["black", "red", "blue"];
 
+//changes color of one of the sub_boards in the left grid
+function colorBoard(board_name, color)
+{
+    for (let box = 0; box < 9; box ++)
+    {
+        document.getElementsByClassName(board_name)[box].style.backgroundColor = color;
+    }
+}
+
+//switches the rightside board to match one of the sub_boards in the left grid
+//also highlights which board is shown on the right side
 function matchBoards(board_name)
 {
-    
-    //reset board color
-    if (boardstate.big_board[0] != "dud")
+
+    //remove previous highlighting
+    if (boardstate.current_board != "")
     {
-        for (let box = 0; box < 9; box++)
-        {
-            document.getElementsByClassName(boardstate.big_board[0])[box].style.backgroundColor = "rgb(110, 209, 143)";
-        }
+        colorBoard(boardstate.current_board, "rgb(110, 209, 143)");
     }
 
-    //reset previous board values
-    current_board = boardstate.big_board;
-    boardstate.full_board[current_board[0]] = current_board[1];
-    
+    //set current board to board_name
+    boardstate.current_board = board_name;
 
-    //pull from new board
-    boardstate.big_board[0] = board_name;
-    boardstate.big_board[1] = boardstate.full_board[board_name];
+    //open new board
+    for (box in boardstate.full_board[boardstate.current_board])
+    {
+        document.getElementsByClassName("side_board")[box].innerHTML = players_map[boardstate.full_board[boardstate.current_board][box]];
+        document.getElementsByClassName("side_board")[box].style.color = player_colors[boardstate.full_board[boardstate.current_board][box]];
+    }
 
-    //highlight new board
+    //Add Highlighting 
     for (let box = 0; box < 9; box++)
     {
-        document.getElementsByClassName(board_name)[box].style.backgroundColor = "rgb(123, 219, 68)";
-    }
-    
-    //update side board
-    for (box in boardstate.big_board[1])
-    {
-        box_value = boardstate.big_board[1][box];
-        document.getElementsByClassName("side_board")[box].innerHTML = players_map[box_value];
-        document.getElementsByClassName("side_board")[box].style.color = player_colors[box_value]
+        colorBoard(board_name, "rgb(123, 219, 68)");
     }
 }
 //
 
 //start of game functions & variables
+
+//waits for the first player to select which sub_board they would like to start on
 function startSelect(board_name)
 {
     matchBoards(board_name);
@@ -110,6 +134,7 @@ function startSelect(board_name)
     }
 }
 
+// Clears board and starts game
 function startGame()
 {
     boardstate.reset();
@@ -126,21 +151,66 @@ function startGame()
 //gameplay functions & variables
 let player_turn = 1;
 
+//Empty function to delay output
+function empty() {}
+
+//Allows users to take turn with X/O with colors red/blue
 function play_turn(box)
 {    
     if (document.getElementsByClassName("side_board")[box].innerHTML=="")
     {
+        //inserts X/O
         document.getElementsByClassName("side_board")[box].innerHTML = players_map[player_turn];
-        document.getElementsByClassName("side_board")[box].style.color = player_colors[player_turn];  
+        document.getElementsByClassName("side_board")[box].style.color = player_colors[player_turn];
+
+        //map to boardstate
+        boardstate.full_board[boardstate.current_board][box] = player_turn;
+
+        //map to full board
+        document.getElementsByClassName(boardstate.current_board)[box].innerHTML = players_map[player_turn];
+        document.getElementsByClassName(boardstate.current_board)[box].style.color = player_colors[player_turn];
+
+        //check for win in sub_board
+        checkSubWin();
+        
+        //switch boxes
+        //setTimeout(matchBoards, 500, board_map[box]);
+        matchBoards(board_map[box]);
+
+        //change player turn
         player_turn = (player_turn % 2) + 1;
     }
-    else
-    {
-        play_turn(box);
-    }
-
 }
- 
+
+//check if any of the sub_boards have been won
+function checkSubWin()
+{
+    board_name = boardstate.current_board;
+    sub_board = boardstate.full_board[board_name];
+    if(boardstate.big_board[board_map[board_name]] == 0)
+    {
+        for (let player = 1; player <= 2; player ++)
+        {
+            if ((player == sub_board[0] && player == sub_board[3] && player == sub_board[6]) ||
+                (player == sub_board[1] && player == sub_board[4] && player == sub_board[7]) ||
+                (player == sub_board[2] && player == sub_board[5] && player == sub_board[8]) ||
+                (player == sub_board[0] && player == sub_board[1] && player == sub_board[2]) ||
+                (player == sub_board[3] && player == sub_board[4] && player == sub_board[5]) ||
+                (player == sub_board[6] && player == sub_board[7] && player == sub_board[8]) ||
+                (player == sub_board[0] && player == sub_board[4] && player == sub_board[8]) ||
+                (player == sub_board[2] && player == sub_board[4] && player == sub_board[6])
+            )
+            {
+
+                const player_colors_highlight = ["", "rgb(250, 108, 82)", "rgb(73, 192, 242)"]
+                boardstate.big_board[board_map[board_name]] = player;
+                document.getElementsByClassName("grid_box")[board_map[board_name]].style.backgroundColor = player_colors_highlight[player];
+            }
+        }
+    }
+}
+
+//Resign button to end game and give oppposition victory
 function resign()
 {
  if(confirm("Are you sure you want to resign?" == true))
