2a55346415f968ec927e8408c43cfc9fd2bfb79d
D1wan <70646501+D1wan@users.noreply.github.com>
2022-12-28 15:04:02 -0500

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,21 +1,21 @@
 //menu functions & variables
 function how_to_play()
 {
-    window.location.href = "/nic-nac-noe/help.html";
+    window.location.href = "help.html";
 }
 
 function home()
 {
-    window.location.href = "/nic-nac-noe/";
+    window.location.href = "index.html";
 }
 
 function openGame()
 {
-    window.location.href = "/nic-nac-noe/game.html";
+    window.location.href = "game.html";
 }
 
 function openMobile()
 {
-    window.location.href = "/nic-nac-noe/mobile_game.html";
+    window.location.href = "mobile_game.html";
 }
 //
